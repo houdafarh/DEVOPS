38417f1ef220f40c1f72d9a4ce86b4af190ccbe2
nourhen-allagui <57333138+nourhen-allagui@users.noreply.github.com>
2021-10-18 13:02:44 +0100

    test unitaire pour les services d'entité Employe

--- TimesheetProject/src/test/java/tn/esprit/spring/test/EmployeTest.java
+++ TimesheetProject/src/test/java/tn/esprit/spring/test/EmployeTest.java
@@ -4,58 +4,77 @@ import static org.junit.Assert.*;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.repository.ContratRepository;
+import tn.esprit.spring.repository.DepartementRepository;
 import tn.esprit.spring.services.EmployeServiceImpl;
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class EmployeTest {
 
 	@Autowired
     EmployeServiceImpl employeServImp;
+	@Autowired
+	DepartementRepository deptRepoistory;
+	@Autowired
+	ContratRepository contratRepository;
 	private static final Logger l = LogManager.getLogger(EmployeServiceImpl.class);
 	
-	
+
 	
 	@Test
 	public void testAjouterEmploye() throws ParseException {
 		Employe emp  = new Employe("kallel","khaled","Khaled.kallel@ssiiconsulting.tn",true,Role.INGENIEUR);
-		//assertTrue(emp.getRole().equals(Role.INGENIEUR));	
-		assertNotNull("it is not empty", emp);
-		l.info("compatible  " + emp);
+		int Id = employeServImp.ajouterEmploye(emp);
+		assertNotNull("it is not empty",Id);
+		l.info("employe a ete ajoute  " + Id);
+		employeServImp.deleteEmployeById(Id);
 		}
 	
 	
-	
 	@Test
-	public void mettreAjourEmailByEmployeId()throws ParseException{
+	public void testMettreAjourEmailByEmployeId()throws ParseException{
 		Employe emp  = new Employe("kallel","khaled","Khaled.kallel@ssiiconsulting.tn",true,Role.INGENIEUR);
-		//Employe emp1  = new Employe("kallel1","khaled1","Khaled.kallel1@ssiiconsulting.tn",true,Role.INGENIEUR);
+		int Id = employeServImp.ajouterEmploye(emp);
+		if(emp.getId()== Id){
 		String mail1="Nourhen.allagui@gmail.com";
 		emp.setEmail(mail1);
 		String mail = emp.getEmail();
+		l.info("it's" + mail);
 		assertTrue(emp.getEmail().equals(mail1));
 		l.info("compatible  " + mail);
-		
+		}
+		else 
+			l.info("sorry");
+		employeServImp.deleteEmployeById(Id);
 	}
 	
 	
 	@Test
 	public void testAffecterEmployeADepartement(){
 		Employe emp  = new Employe("kallel","khaled","Khaled.kallel@ssiiconsulting.tn",true,Role.INGENIEUR);
-		emp.setId(1);
+		int id = employeServImp.ajouterEmploye(emp);
 	    Departement dep = new Departement("informatique");
-	    dep.setId(1);
-	    if(dep.getEmployes()== null){
-	    	ArrayList<Employe> lemp = new ArrayList();
+	    deptRepoistory.save(dep);
+	    int Idd = dep.getId();
+	    if(dep.getEmployes() == null){
+	    	ArrayList<Employe> lemp = new ArrayList<Employe>();
 	    	lemp.add(emp);
 	    	dep.setEmployes(lemp);
 	    	assertNotNull("it is not empty", dep.getEmployes());
@@ -68,8 +87,9 @@ public class EmployeTest {
 	    else {
 	    dep.getEmployes().add(emp);
 	    assertNotNull(dep);
-	    l.debug("good " +dep.getEmployes());
 	    }
+	    deptRepoistory.deleteById(Idd);
+	    employeServImp.deleteEmployeById(id);
 	}
 	
 	
@@ -77,13 +97,13 @@ public class EmployeTest {
 	@Test
 	public void testDesaffecterEmployeDuDepartement(){
 		Departement dep = new Departement("informatique");
-		ArrayList<Employe> lemp = new ArrayList();
+		ArrayList<Employe> lemp = new ArrayList<Employe>();
 		Employe emp  = new Employe("kallel","khaled","Khaled.kallel@ssiiconsulting.tn",true,Role.INGENIEUR);
-		emp.setId(1);
-		int employeId = emp.getId();
+		int employeId = employeServImp.ajouterEmploye(emp);
 		lemp.add(emp);
 		dep.setEmployes(lemp);
 		int employeNb = dep.getEmployes().size();
+		System.out.println(employeNb);
 		for(int index = 0; index < employeNb; index++){
 			if(dep.getEmployes().get(index).getId() == employeId){
 				dep.getEmployes().remove(index);
@@ -93,46 +113,44 @@ public class EmployeTest {
 				
 			}
 		}
-		
+		deptRepoistory.deleteAll();
+		employeServImp.deleteEmployeById(employeId);
 	}
 	
 	
 	@Test
 	public void testAjouterContrat(){
-		Contrat cont = new Contrat("10/10/2021","CDI", 120.3f);
-		cont.setReference(1);
-		int contrat =cont.getReference();
-        assertEquals(contrat, 1);
-        l.info("reference not null", +contrat);
+		Date d=new Date(12-01-2021);
+		Contrat cont = new Contrat(d,"CDI", 120.3f);
+		contratRepository.save(cont);
+		int id = cont.getReference();
+        assertEquals(cont.getReference(), id);
+        l.info("reference not null", +id);
+        contratRepository.deleteAll();
 	}
 	
 	@Test
 	public void testAffecterContratAEmploye(){
-		Contrat cont = new Contrat("10/10/2021","CDI", 120.3f);
+		Date d=new Date(12-01-2021);
+		Contrat cont = new Contrat(d,"CDI", 120.3f);
 		Employe emp  = new Employe("kallel","khaled","Khaled.kallel@ssiiconsulting.tn",true,Role.INGENIEUR);
+		int empId = employeServImp.ajouterEmploye(emp);
 		cont.setEmploye(emp);
 		assertTrue("affecté", cont.getEmploye() != null);
 		l.info("affecté", cont.getEmploye());
+		contratRepository.deleteAll();
+		employeServImp.deleteEmployeById(empId);
 	}
 	
 	@Test
 	public void testGetEmployePrenomById(){
 		Employe emp  = new Employe("kallel","khaled","Khaled.kallel@ssiiconsulting.tn",true,Role.INGENIEUR);
-		emp.setId(1);
-		if(emp.getId()== 1){
+		int Id = employeServImp.ajouterEmploye(emp);
+		if(Id!= 0){
 		assertTrue(emp.getPrenom() != null);
+		l.info("Prenom :", emp.getPrenom());
 		}
+		employeServImp.deleteEmployeById(Id);;
 	}
-	/*
-	@Test
-	public void testGetNombreEmployeJPQL(){
-		System.out.println(employeServImp.getNombreEmployeJPQL());
-		
-	}*/
-	/*
-	@Test
-	public void testDeleteAllContratJPQL(){
-		employeServImp.deleteAllContratJPQL();
-	}*/
-
+	
 }
