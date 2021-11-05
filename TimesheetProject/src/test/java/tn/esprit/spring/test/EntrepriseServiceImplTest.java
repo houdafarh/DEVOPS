fd716f7f2b917332dd5d1542f50ceb1d0be3a1dd
Huda Farh <57289357+houdafarh@users.noreply.github.com>
2021-11-05 16:24:38 +0100

    Update EntrepriseServiceImplTest.java

--- TimesheetProject/src/test/java/tn/esprit/spring/test/EntrepriseServiceImplTest.java
+++ TimesheetProject/src/test/java/tn/esprit/spring/test/EntrepriseServiceImplTest.java
@@ -4,7 +4,8 @@ package tn.esprit.spring.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
 import java.util.List;
 
