43b5aabdb5441909c6bff2b575b8a3755bc75efb
nourhen-allagui <57333138+nourhen-allagui@users.noreply.github.com>
2021-11-08 20:13:36 +0100

    Update RestControlEntreprise.java

--- TimesheetProject/src/main/java/tn/esprit/spring/controller/RestControlEntreprise.java
+++ TimesheetProject/src/main/java/tn/esprit/spring/controller/RestControlEntreprise.java
@@ -8,33 +8,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Entreprise;
-import tn.esprit.spring.services.IEmployeService;
 import tn.esprit.spring.services.IEntrepriseService;
-import tn.esprit.spring.services.ITimesheetService;
 
 @RestController
 public class RestControlEntreprise {
 
-	
-	@Autowired
-	IEmployeService iemployeservice;
 	@Autowired
 	IEntrepriseService ientrepriseservice;
-	@Autowired
-	ITimesheetService itimesheetservice;
 	
-	// Ajouter Entreprise : http://localhost:8081/SpringMVC/servlet/ajouterEntreprise
-	//{"id":1,"name":"SSII Consulting","raisonSocial":"Cite El Ghazela"}
+	// http://localhost:8081/SpringMVC/servlet/ajouterEntreprise
 
 	@PostMapping("/ajouterEntreprise")
 	@ResponseBody
-	public int ajouterEntreprise() {
-		Entreprise ssiiConsulting = new Entreprise();
+	public int ajouterEntreprise(@RequestBody Entreprise ssiiConsulting) {
 		ientrepriseservice.ajouterEntreprise(ssiiConsulting);
 		return ssiiConsulting.getId();
 	}
@@ -61,12 +53,11 @@ public class RestControlEntreprise {
 		return ientrepriseservice.getEntrepriseById(entrepriseId);
 	}
     
-    // http://localhost:8081/SpringMVC/servlet/ajouterDepartements
+    // http://localhost:8081/SpringMVC/servlet/ajouterDepartement
 
  	@PostMapping("/ajouterDepartement")
  	@ResponseBody
-	public int ajouterDepartement() {
- 		Departement dep = new Departement();
+	public int ajouterDepartement(@RequestBody Departement dep) {
 		return ientrepriseservice.ajouterDepartement(dep);
 	}
 	
